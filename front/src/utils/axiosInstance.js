cc7f5f7ed3d683cdc1e1012ea1100ec9be68bfb5
asma1mokeddes <amokeddes@myges.fr>
2024-05-31 20:28:09 +0200

    fix HOST_API on axiosInstance.js

--- front/src/utils/axiosInstance.js
+++ front/src/utils/axiosInstance.js
@@ -1,20 +1,20 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000",
+	baseURL: "https://tic-tac-toe-server-thgx.onrender.com",
 });
 
 axiosInstance.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      config.headers["Authorization"] = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
+	(config) => {
+		const token = localStorage.getItem("token");
+		if (token) {
+			config.headers["Authorization"] = `Bearer ${token}`;
+		}
+		return config;
+	},
+	(error) => {
+		return Promise.reject(error);
+	}
 );
 
 export default axiosInstance;
